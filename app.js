2f43f73af4d12b64effe6c508a8d6ec57c25cb4b
ModestasSinkevicius1 <modestas.sinkevicius1@gmail.com>
2022-09-13 11:29:45 +0300

    sql

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use(
 );
 app.use(express.json());
 
+//express settings
 const con = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -24,8 +25,70 @@ app.get('/', (req, res) => {
 })
 
 app.get('/bye', (req, res) => {
-    res.send('Goodbye World!')
-  })
+  res.send('Goodbye World!')
+})
+
+
+//READ
+//Select column1, column2, ...
+//From table_name;
+
+//returns JSON string
+app.get('/trees', (req, res) => {
+  const sql = `
+  SELECT * FROM medis
+  `;
+  con.query(sql, (err, result) => {
+      if (err) throw err;
+      res.send(result);
+  });
+})
+
+app.get('/trees/palmes', (req, res) => {
+  const sql = `
+  SELECT * FROM medis
+  WHERE type = 1
+  `;
+  con.query(sql, (err, result) => {
+      if (err) throw err;
+      res.send(result);
+  });
+})
+
+app.get('/trees/mix', (req, res) => {
+  const sql = `
+  SELECT * FROM medis
+  WHERE type = 1 OR type = 3
+  `;
+  con.query(sql, (err, result) => {
+      if (err) throw err;
+      res.send(result);
+  });
+})
+
+app.get('/trees/height', (req, res) => {
+  const sql = `
+  SELECT * FROM medis
+  WHERE height > 6
+  ORDER BY type, height DESC
+  `;
+  con.query(sql, (err, result) => {
+      if (err) throw err;
+      res.send(result);
+  });
+})
+
+app.get('/trees/by/:type', (req, res) => {
+  const sql = `
+  SELECT * FROM medis
+  WHERE type = ?
+  ORDER BY title DESC
+  `;
+  con.query(sql, [req.params.type, req.query.sort === 'title' ? 'title' : 'height'], (err, result) => {
+      if (err) throw err;
+      res.send(result);
+  });
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
